22e988bd31704b78f3e0807d4a8a0eb2c82c9bbe
Poschi <development@poschi.net>
2018-04-22 11:31:14 +0200

    Arrow Functions und Logo

---
+++ src/arrow.ts
@@ -0,0 +1,24 @@
+class Hackerspace {
+    constructor(private name: string, private users: string[]) { }
+    public greetAll(){
+        this.users.forEach(function(user: string) {
+            console.log(user + " in " + this.name); // Error
+        });
+    }
+
+    // public greetAll(){
+    //     const _this = this;
+    //     this.users.forEach(function(user: string) {
+    //         console.log(user + " in " + _this.name);
+    //     });
+    // }
+
+    // public greetAll(){
+    //     this.users.forEach((user) => {
+    //         console.log(user + " in " + this.name);
+    //     });
+    // }
+}
+
+const space = new Hackerspace("K4CG", ["Poschi", "Zeitreisender"]);
+space.greetAll();
